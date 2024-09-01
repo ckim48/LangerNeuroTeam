6cb62774382570e7d89e02200f0d565a56a90269
ckim48 <madckkim@gmail.com>
2024-09-01 22:34:54 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,80 @@
+# Flask --> Open-source Python library which allows you to make Web-application with Python
+# every html goes to folder 'templates'
+# every css/js + images files goes to 'static'
+from flask import Flask, render_template, request, url_for, redirect, flash, session # We are going to use Flask for this python code
+from datetime import timedelta
+import firebase_admin
+from firebase_admin import credentials, auth
+
+# Loading the secret key
+cred = credentials.Certificate("secret.json")
+# Access to the firebase using the loaded key.
+firebase_admin.initialize_app(cred)
+
+app = Flask(__name__)  # It creates an empty web application
+app.secret_key = "abc"
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=5)
+
+# Log-out
+# Session = {}
+# As soon as user login
+# Session = {"username" : "test"}
+
+# Added the homepage --> For now we don't html filr for the homepage, but we just have "Hello, World"
+@app.route('/')
+def index():
+    isLogin = False
+    if 'username' in session:
+        isLogin = True
+    return render_template('index.html', isLogin = isLogin)
+
+# We added the login page to our web application
+@app.route('/login' , methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        try:
+            user = auth.get_user_by_email(username)
+            session["username"] = username
+            return redirect(url_for('index'))
+        except firebase_admin._auth_utils.UserNotFoundError:
+            flash("Username or password is wrong")
+            return render_template('login.html')
+        except Exception as e:
+            flash(str(e))
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/register' , methods = ["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        try:
+            user = auth.create_user(
+                email = username,
+                password=password
+            )
+            return redirect(url_for('login'))
+        except firebase_admin._auth_utils.EmailAlreadyExistsError:
+            flash("This email is already registered")
+            return render_template('register.html')
+        except Exception as e:
+            flash(str(e))
+            return render_template('register.html')
+    else:
+        return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+@app.route('/task')
+def task():
+    return render_template('task1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000) # run our web application
